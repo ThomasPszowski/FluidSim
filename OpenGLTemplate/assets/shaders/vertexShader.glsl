68c9ea109010195683fb372d7f94d4ad5b073014
ThomasPszowski <162191217+ThomasPszowski@users.noreply.github.com>
2025-04-29 22:09:11 +0200

    SImC

--- OpenGLTemplate/assets/shaders/vertexShader.glsl
+++ OpenGLTemplate/assets/shaders/vertexShader.glsl
@@ -1,10 +1,10 @@
-﻿#version 330 core
-layout (location = 0) in vec2 aPos;  // Pozycja 2D punktu
-layout (location = 1) in vec4 aColor; // Color
+#version 330 core
+layout (location = 0) in vec2 aPos;  
+layout (location = 1) in vec4 aColor; 
 
 uniform vec2 center;
 uniform float zoom;
-uniform float pointSize; // Rozmiar punktu
+uniform float pointSize; 
 
 out vec4 color;
 
@@ -12,5 +12,5 @@ void main() {
     vec2 pos = (aPos - center) * zoom;
     gl_Position = vec4(pos, 0.0, 1.0);
     color = aColor;
-    gl_PointSize = pointSize;  // Ustawianie rozmiaru punktu
+    gl_PointSize = pointSize;  
 }
--- OpenGLTemplate/src/main.cpp
+++ OpenGLTemplate/src/main.cpp
@@ -2,12 +2,15 @@
 using namespace Engine;
 
 void terminateGLFW();
+#define PARTICLE_COUNT 1000
 
-const int MAX_POINTS = 200;
+
+const int MAX_POINTS = PARTICLE_COUNT;
 GLuint pointsVAO, pointsVBO;
 std::vector<Point2D> points(MAX_POINTS);
 
 void simulation_thread();
+void SimC_simulation_thread();
 
 bool isRunning = true;
 
@@ -16,7 +19,7 @@ int main() {
     const bool fullScreenMode = false;
     float pointSize = 4; // Możesz zmieniać ten rozmiar dynamicznie
 
-    std::thread watek(simulation_thread);
+    std::thread watek(SimC_simulation_thread);
     //watek.join();
 
     if (!Window::createWindow(windowWidth, windowHeight, "OpenGL Points", fullScreenMode)) return -1;
@@ -106,7 +109,7 @@ void simulation_thread() {
     using namespace std;
     WaterSim sim(MAX_POINTS);
     sim.step_size = 0.001;
-    sim.velocity_damping = 1;
+    sim.velocity_damping = 0.99;
     sim.collision_smoothness = 100;
     sim.collision_strength = 0.5;
 	sim.same_position_collision_strength = 1;
@@ -117,7 +120,7 @@ void simulation_thread() {
 
     sim.ArrangeParticlesSquare();
     
-    float ups = 100. / sim.step_size;
+    float ups = 1000. / sim.step_size;
 	int u_delay = 1000 / ups;
 	auto u_last = chrono::high_resolution_clock::now();
 
@@ -145,3 +148,181 @@ void simulation_thread() {
     }
 
 }
+
+#define GRID_SIZE 32
+
+#define DT 0.01f
+#define GRAVITY_MAGNITUDE -9.8f
+
+typedef struct {
+    float x, y;
+} Vec2;
+
+typedef struct {
+    Vec2 pos;
+    Vec2 vel;
+} Particle;
+
+typedef struct {
+    Vec2 velocity;
+    float mass;
+} GridCell;
+
+Particle particles[PARTICLE_COUNT];
+GridCell grid[GRID_SIZE][GRID_SIZE];
+
+void init_particles() {
+    for (int i = 0; i < PARTICLE_COUNT; i++) {
+        particles[i].pos.x = 10.0f + (float)rand() / RAND_MAX * GRID_SIZE;
+        particles[i].pos.y = 20.0f + (float)rand() / RAND_MAX * GRID_SIZE;
+        particles[i].vel.x = 0.0f;
+        particles[i].vel.y = 0.0f;
+    }
+}
+
+void clear_grid() {
+    for (int y = 0; y < GRID_SIZE; y++)
+        for (int x = 0; x < GRID_SIZE; x++) {
+            grid[y][x].velocity = { 0.0f, 0.0f };
+            grid[y][x].mass = 0.0f;
+        }
+}
+
+void particle_to_grid() {
+    for (int i = 0; i < PARTICLE_COUNT; i++) {
+        Particle* p = &particles[i];
+        int x = (int)p->pos.x;
+        int y = (int)p->pos.y;
+
+        if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
+            grid[y][x].velocity.x += p->vel.x;
+            grid[y][x].velocity.y += p->vel.y;
+            grid[y][x].mass += 1.0f;
+        }
+    }
+
+    for (int y = 0; y < GRID_SIZE; y++)
+        for (int x = 0; x < GRID_SIZE; x++) {
+            if (grid[y][x].mass > 0.0f) {
+                grid[y][x].velocity.x /= grid[y][x].mass;
+                grid[y][x].velocity.y /= grid[y][x].mass;
+            }
+        }
+}
+
+Vec2 gravity = { 0.0f, -9.8f };  
+
+void apply_gravity() {
+    for (int y = 0; y < GRID_SIZE; y++) {
+        for (int x = 0; x < GRID_SIZE; x++) {
+            if (grid[y][x].mass > 0.0f) {
+                grid[y][x].velocity.x += gravity.x * DT;
+                grid[y][x].velocity.y += gravity.y * DT;
+            }
+        }
+    }
+}
+
+void set_gravity_direction(Vec2 dir) {
+    float len = sqrtf(dir.x * dir.x + dir.y * dir.y);
+    if (len > 1e-6f) {
+        gravity.x = dir.x / len * GRAVITY_MAGNITUDE;
+        gravity.y = dir.y / len * GRAVITY_MAGNITUDE;
+    }
+    else {
+        gravity.x = 0.0f;
+        gravity.y = 0.0f;
+    }
+}
+
+void grid_to_particle() {
+    for (int i = 0; i < PARTICLE_COUNT; i++) {
+        Particle* p = &particles[i];
+        int x = (int)p->pos.x;
+        int y = (int)p->pos.y;
+
+        if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
+            Vec2 gridVel = grid[y][x].velocity;
+            p->vel = gridVel;
+        }
+    }
+}
+
+void advect_particles() {
+    for (int i = 0; i < PARTICLE_COUNT; i++) {
+        Particle* p = &particles[i];
+
+        p->pos.x += p->vel.x * DT;
+        p->pos.y += p->vel.y * DT;
+
+        if (p->pos.x < 0) { p->pos.x = 0; p->vel.x *= -0.5f; }
+        if (p->pos.x >= GRID_SIZE) { p->pos.x = GRID_SIZE - 0.1; p->vel.x *= -0.5f; }
+
+        if (p->pos.y < 0) { p->pos.y = 0; p->vel.y *= -0.5f; }
+        if (p->pos.y >= GRID_SIZE) { p->pos.y = GRID_SIZE - 0.1; p->vel.y *= -0.5f; }
+    }
+}
+
+void apply_repulsion() {
+    const float minDist = 2.0f;
+    const float k = 10.0f; 
+    const float eps = 0.001f;
+
+    for (int i = 0; i < PARTICLE_COUNT; i++) {
+        for (int j = i + 1; j < PARTICLE_COUNT; j++) {
+            Vec2 dp = {
+                particles[i].pos.x - particles[j].pos.x,
+                particles[i].pos.y - particles[j].pos.y
+            };
+            float dist2 = dp.x * dp.x + dp.y * dp.y;
+            if (dist2 < eps) {
+                particles[i].vel.x *= -1;
+                particles[i].vel.y *= -1;
+            }
+            if (dist2 < minDist * minDist) {
+                float dist = sqrtf(dist2);
+                float force = k * (minDist - dist) / dist;
+
+                Vec2 dir = { dp.x / dist, dp.y / dist };
+                particles[i].vel.x += dir.x * force * DT;
+                particles[i].vel.y += dir.y * force * DT;
+                particles[j].vel.x -= dir.x * force * DT;
+                particles[j].vel.y -= dir.y * force * DT;
+            }
+
+        }
+    }
+}
+
+
+void step_simulation() {
+    clear_grid();
+    particle_to_grid();
+    apply_gravity();
+    grid_to_particle();
+    apply_repulsion();
+    advect_particles();
+}
+
+void SimC_simulation_thread() {
+
+    init_particles();
+    Vec2 gravityDirection = { 0.0f, 1.0f };
+
+    while (isRunning) {
+        
+        for (int i = 0; i < PARTICLE_COUNT; i++) {
+			points[i].x = particles[i].pos.x / 32.;
+			points[i].y = particles[i].pos.y / 32.;
+		}
+        //std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        gravityDirection.x = Engine::Input::normX;
+        gravityDirection.y = Engine::Input::normY;
+        set_gravity_direction(gravityDirection);
+        step_simulation();
+        
+	}
+}
+
+
